430093076d06e2a23a309e903bf246653fb3397e
Noah Kritz <noahkritz@gmail.com>
2020-04-14 12:58:00 -0400

    merged with kasem's master

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/kritz/Playground/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/kritz/Playground/receive_sharing_intent/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/kritz/Playground/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
