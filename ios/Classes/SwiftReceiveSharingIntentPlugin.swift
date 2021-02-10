e8d0d3a1653e8b4970b9836c7de1b6ee6e804aa5
Dan Reynolds <me@danreynolds.ca>
2021-02-09 23:25:20 -0500

    fix comment

--- ios/Classes/SwiftReceiveSharingIntentPlugin.swift
+++ ios/Classes/SwiftReceiveSharingIntentPlugin.swift
@@ -90,7 +90,7 @@ public class SwiftReceiveSharingIntentPlugin: NSObject, FlutterPlugin, FlutterSt
     // This is the function called on resuming the app from a shared link.
     // It handles requests to open a resource by a specified URL. Returning true means that it was handled successfully, false means the attempt to open the resource failed.
     // If the URL includes the module's ShareMedia prefix, then we process the URL and return true if we know how to handle that kind of URL or false if we are not able to.
-    // If the URL does not include the module's prefix, then we return false to indicate our module attempt to open the resource failed and others should be allowed to.
+    // If the URL does not include the module's prefix, then we return false to indicate our module's attempt to open the resource failed and others should be allowed to.
     // Reference: https://developer.apple.com/documentation/uikit/uiapplicationdelegate/1623112-application
     public func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
         if (hasMatchingSchemePrefix(url: url)) {
