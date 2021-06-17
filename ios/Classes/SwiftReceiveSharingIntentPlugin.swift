aee4b7c77901c2cd18d7fb4970837d83969f320f
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2021-06-17 17:27:16 +0300

    read custom group ID in SwiftReceiveSharingIntentPlugin.swift

--- ios/Classes/SwiftReceiveSharingIntentPlugin.swift
+++ ios/Classes/SwiftReceiveSharingIntentPlugin.swift
@@ -121,7 +121,8 @@ public class SwiftReceiveSharingIntentPlugin: NSObject, FlutterPlugin, FlutterSt
     private func handleUrl(url: URL?, setInitialData: Bool) -> Bool {
         if let url = url {
             let appDomain = Bundle.main.bundleIdentifier!
-            let userDefaults = UserDefaults(suiteName: "group.\(appDomain)")
+            let appGroupId = (Bundle.main.object(forInfoDictionaryKey: "AppGroupId") as? String) ?? "group.\(Bundle.main.bundleIdentifier!)"
+            let userDefaults = UserDefaults(suiteName: appGroupId)
             if url.fragment == "media" {
                 if let key = url.host?.components(separatedBy: "=").last,
                     let json = userDefaults?.object(forKey: key) as? Data {
