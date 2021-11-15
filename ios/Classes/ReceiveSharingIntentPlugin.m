b5c8e6888da67859b3835ca79bda535b62da657c
korvan <kor123@list.ru>
2021-11-15 12:51:46 +0300

    Fixed build for iOS if use_frameworks is disabled
    https://github.com/KasemJaffer/receive_sharing_intent/issues/195
    If use_frameworks is disabled in Podfile Swift.h
    header should be included with relative path,
    without framework name prefix

--- ios/Classes/ReceiveSharingIntentPlugin.m
+++ ios/Classes/ReceiveSharingIntentPlugin.m
@@ -1,5 +1,9 @@
 #import "ReceiveSharingIntentPlugin.h"
+#if __has_include(<receive_sharing_intent/receive_sharing_intent-Swift.h>)
 #import <receive_sharing_intent/receive_sharing_intent-Swift.h>
+#else
+#import "receive_sharing_intent-Swift.h"
+#endif
 
 @implementation ReceiveSharingIntentPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
