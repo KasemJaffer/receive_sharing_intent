ec955cbb505eb54e9223e76fc5352c06b09f4a91
krabbenprgr <masterkrebs98@gmail.com>
2021-11-18 13:53:35 +0100

    added possibility to share .txt -files

--- android/src/main/kotlin/com/kasem/receive_sharing_intent/ReceiveSharingIntentPlugin.kt
+++ android/src/main/kotlin/com/kasem/receive_sharing_intent/ReceiveSharingIntentPlugin.kt
@@ -112,9 +112,9 @@ class ReceiveSharingIntentPlugin : FlutterPlugin, ActivityAware, MethodCallHandl
 
     private fun handleIntent(intent: Intent, initial: Boolean) {
         when {
-            (intent.type?.startsWith("text") != true)
+            (intent.type?.startsWith("text") != true || intent.hasExtra(Intent.EXTRA_STREAM))
                     && (intent.action == Intent.ACTION_SEND
-                    || intent.action == Intent.ACTION_SEND_MULTIPLE) -> { // Sharing images or videos
+                    || intent.action == Intent.ACTION_SEND_MULTIPLE) -> { // Sharing images or videos or .txt
 
                 val value = getMediaUris(intent)
                 if (initial) initialMedia = value
