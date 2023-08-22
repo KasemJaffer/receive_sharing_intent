6ad8f582d7bc7e2fd5a258399e7dbe8c39245d32
Nahrawene Ben Hmida <nahrawene.benhmida@expensya.com>
2023-08-22 11:42:39 +0100

    update version from pub.dev

--- ios/Classes/SwiftReceiveSharingIntentPlugin.swift
+++ ios/Classes/SwiftReceiveSharingIntentPlugin.swift
@@ -18,12 +18,10 @@ public class SwiftReceiveSharingIntentPlugin: NSObject, FlutterPlugin, FlutterSt
     private var eventSinkMedia: FlutterEventSink? = nil;
     private var eventSinkText: FlutterEventSink? = nil;
     
-    // Singleton is required for calling functions directly from AppDelegate
-    // - it is required if the developer is using also another library, which requires to call "application(_:open:options:)"
-    // -> see Example app
-    public static let instance = SwiftReceiveSharingIntentPlugin()
     
     public static func register(with registrar: FlutterPluginRegistrar) {
+        let instance = SwiftReceiveSharingIntentPlugin()
+        
         let channel = FlutterMethodChannel(name: kMessagesChannel, binaryMessenger: registrar.messenger())
         registrar.addMethodCallDelegate(instance, channel: channel)
         
@@ -54,11 +52,9 @@ public class SwiftReceiveSharingIntentPlugin: NSObject, FlutterPlugin, FlutterSt
         }
     }
 
-    // By Adding bundle id to prefix, we'll ensure that the correct application will be openned
-    // - found the issue while developing multiple applications using this library, after "application(_:open:options:)" is called, the first app using this librabry (first app by bundle id alphabetically) is opened
     public func hasMatchingSchemePrefix(url: URL?) -> Bool {
-        if let url = url, let appDomain = Bundle.main.bundleIdentifier {
-            return url.absoluteString.hasPrefix("\(self.customSchemePrefix)-\(appDomain)")
+        if let url = url {
+            return url.absoluteString.hasPrefix(self.customSchemePrefix)
         }
         return false
     }
@@ -121,8 +117,7 @@ public class SwiftReceiveSharingIntentPlugin: NSObject, FlutterPlugin, FlutterSt
     private func handleUrl(url: URL?, setInitialData: Bool) -> Bool {
         if let url = url {
             let appDomain = Bundle.main.bundleIdentifier!
-            let appGroupId = (Bundle.main.object(forInfoDictionaryKey: "AppGroupId") as? String) ?? "group.\(Bundle.main.bundleIdentifier!)"
-            let userDefaults = UserDefaults(suiteName: appGroupId)
+            let userDefaults = UserDefaults(suiteName: "group.\(appDomain)")
             if url.fragment == "media" {
                 if let key = url.host?.components(separatedBy: "=").last,
                     let json = userDefaults?.object(forKey: key) as? Data {
