1ea4588037f2cc4d10acf59d83f3d92265e9e306
Lakesyd5 <adeleoje@gmail.com>
2026-06-04 22:45:41 +0100

    fix(ios): resolve Flutter.h header not found on simulator builds

--- ios/Classes/ReceiveSharingIntentPlugin.h
+++ ios/Classes/ReceiveSharingIntentPlugin.h
@@ -1,4 +1,8 @@
+#if __has_include(<Flutter/Flutter.h>)
 #import <Flutter/Flutter.h>
+#elif __has_include(<FlutterMacOS/FlutterMacOS.h>)
+#import <FlutterMacOS/FlutterMacOS.h>
+#endif
 
 @interface ReceiveSharingIntentPlugin : NSObject<FlutterPlugin>
 @end
